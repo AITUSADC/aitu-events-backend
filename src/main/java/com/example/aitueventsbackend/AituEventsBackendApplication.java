cf3e10d3ea7cfb9c7c2380c51a8a559d24246ba6
Akendo <ruslankadirov1337@gmail.com>
2026-03-18 18:44:14 +0500

    init

---
+++ src/main/java/com/example/aitueventsbackend/AituEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitueventsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AituEventsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AituEventsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aitueventsbackend/AituEventsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitueventsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AituEventsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
