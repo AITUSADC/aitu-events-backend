ca276b3c50c733a1ffb4a4989a8ace64c080031d
Shahd Jawas <shjawas4@gmail.com>
2026-03-21 11:53:53 +0500

    feat(user): add User service

---
+++ src/main/java/com/example/aitueventsbackend/services/UserService.java
@@ -0,0 +1,92 @@
+package com.example.aitueventsbackend.services;
+
+import com.example.aitueventsbackend.exceptions.UserAlreadyExistException;
+import com.example.aitueventsbackend.exceptions.UserNotFoundException;
+import com.example.aitueventsbackend.model.Role;
+import com.example.aitueventsbackend.model.User;
+import com.example.aitueventsbackend.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepo userRepo;
+
+    public User create(Long telegramId, String username, String firstName, String lastName, Role role) {
+
+        if (userRepo.existsByTelegramId(telegramId)) {
+            throw new UserAlreadyExistException(telegramId);
+        }
+
+        User user = User.createUser(telegramId, username, firstName, lastName, role);
+
+        return userRepo.save(user);
+    }
+
+    public Page<User> getAll(Pageable pageable) {
+        return userRepo.findAll(pageable);
+    }
+
+    public User getById(Long id) {
+        return userRepo.findById(id).orElseThrow(() -> new UserNotFoundException());
+    }
+
+
+
+    public User fullUpdate(Long id, Long telegramId, String username, String firstName, String lastName, Role role) {
+        User user = userRepo.findById(id).orElseThrow(UserNotFoundException::new);
+
+        // check if new telegram id already exist
+        if (!user.getTelegramId().equals(telegramId) && userRepo.existsByTelegramId(telegramId)) {
+            throw new UserAlreadyExistException(telegramId);
+        }
+
+        user.setTelegramId(telegramId);
+        user.setUsername(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setRole(role);
+
+        return userRepo.save(user);
+    }
+
+    public User partialUpdate(Long id, Long telegramId, String username, String firstName, String lastName, Role role) {
+        User user = userRepo.findById(id).orElseThrow(UserNotFoundException::new);
+
+        // check if new telegram id already exist
+        if (telegramId != null
+                && !user.getTelegramId().equals(telegramId)
+                && userRepo.existsByTelegramId(telegramId)) {
+
+            throw new UserAlreadyExistException(telegramId);
+        }
+
+        if (telegramId != null) {
+            user.setTelegramId(telegramId);
+        }
+        if (username != null) {
+            user.setUsername(username);
+        }
+        if (firstName != null) {
+            user.setFirstName(firstName);
+        }
+        if (lastName != null) {
+            user.setLastName(lastName);
+        }
+        if (role != null) {
+            user.setRole(role);
+        }
+
+        return userRepo.save(user);
+    }
+
+    public void delete (Long id) {
+        if (!userRepo.existsById(id)) {
+            throw new UserNotFoundException();
+        }
+        userRepo.deleteById(id);
+    }
+}
